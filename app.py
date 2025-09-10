22a5db4649694c53381113d0fc19c2343baea383
Henrique Oliveira <riquesantosoliveira@gmail.com>
2025-09-09 22:25:55 -0300

    Dockerizando a aplicação

--- app.py
+++ app.py
@@ -4,17 +4,16 @@ from controller.route import setup_routes
 from models.database import db
 
 app = Flask(__name__)
-app.secret_key = 'projeto-API'
-swagger = Swagger(app)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'projeto-API'
 
+swagger = Swagger(app)
 db.init_app(app)
 setup_routes(app)
 
 with app.app_context():
     db.create_all()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
