5f3cdca9f65d30652abc47d62d8a8a865d1ed79a
yurigabrielconceicaog@gmail.com <yurigabrielconceicaog@gmail.com>
2025-10-22 17:05:28 -0300

    Criação dos métodos PUT e POST na rota de Atividades

--- atividades-notas/config.py
+++ atividades-notas/config.py
@@ -1,6 +1,8 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/atividades_notas.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///" + os.path.join(basedir, "atividades-notas.db"))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24)
+    GERENCIAMENTO_BASE_URL = os.getenv("GERENCIAMENTO_BASE_URL", "http://gerenciamento:5000")
--- atividades-notas/controller/route.py
+++ atividades-notas/controller/route.py
@@ -2,6 +2,7 @@ from flask import request, jsonify
 from models.database import db
 from models.atividades_model import Atividades
 from models.notas_model import Notas
+from datetime import datetime
 
 
 def setup_routes(app):
@@ -50,18 +51,50 @@ def setup_routes(app):
     def create_atividade():
         """"""
 
-        data = request.get_json()
+        dados = request.get_json()
+
+        try:
+            data_entrega = datetime.strptime(dados['data_entrega'], '%Y-%m-%d').date()
+        except ValueError:
+            return jsonify({'erro': 'Formato de data inválido! Digite no seguinte formato: AAAA-MM-DD. Exemplo: 2025-10-25'}), 400
 
         atv = Atividades(
-            nome_atividade=data['nome_atividade'],
-            descricao=data['descricao'],
-            peso_porcento=float(data['peso_porcento']),
-            data_entrega= '',
-            turma_id=int(data['turma_id']),
-            professor_id=int(data['professor_id'])
+            nome_atividade=dados['nome_atividade'],
+            descricao=dados['descricao'],
+            peso_porcento=float(dados['peso_porcento']),
+            data_entrega=data_entrega,
+            turma_id=int(dados['turma_id']),
+            professor_id=int(dados['professor_id'])
         )
 
         db.session.add(atv)
         db.session.commit()
 
         return jsonify({'mensagem': 'Atividade criada!', 'id': atv.id}), 201
+    
+    @app.route('/atividades/<int:id>', methods=['PUT'])
+    def update_atividade(id):
+        """"""
+
+        atv = Atividades.query.get(id)
+
+        if not atv:
+            return jsonify({'erro': 'Atividade não encontrada!'}), 404
+        
+        dados = request.get_json()
+
+        try:
+            data_entrega = datetime.strptime(dados['data_entrega'], '%Y-%m-%d').date()
+        except ValueError:
+            return jsonify({'erro': 'Formato de data inválido! Digite no seguinte formato: AAAA-MM-DD. Exemplo: 2025-10-25'}), 400
+
+        atv.nome_atividade = dados['nome_atividade']
+        atv.descricao = dados['descricao']
+        atv.peso_porcento = float(dados['peso_porcento'])
+        atv.data_entrega = data_entrega
+        atv.turma_id = dados['turma_id']
+        atv.professor_id = dados['professor_id']
+
+        db.session.commit()
+
+        return jsonify({'mensagem': 'Atividade atualizada com sucesso!'}), 200
